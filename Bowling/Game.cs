7b64b0f953de23696baeae29f04bb9aa00c41135
Ozir1z <robbertdewilde@gmail.com>
2023-08-19 12:34:20 -0600

    add frames

--- Bowling/Game.cs
+++ Bowling/Game.cs
@@ -9,10 +9,51 @@ namespace Bowling
     public class Game
     {
         public int Score { get; set; }
+        public int CurrentFrame { get; set; } = 1;
+        private readonly List<Frame> _frames = new(10);
 
+        public Game()
+        {
+            for(int i = 0; i < 10 ; i++)
+            {
+                _frames.Add(new Frame());
+            }
+        }
         public void Roll(int pins)
         {
-            Score += pins;
+            var currentFrame = _frames[CurrentFrame - 1];
+            Score += currentFrame.Roll(pins);
+
+            if (currentFrame.IsFrameDone())
+                CurrentFrame++;
         }
+    }
+
+    public class Frame
+    {
+        public int CurrentRole = 1;
+
+        private int _roll1 = 0;
+        private int _roll2 = 0;
+
+        public int Roll(int pins)
+        {
+            if (CurrentRole > 2) return -1; // make custom OutOfFrameException
+
+            if(CurrentRole == 1)
+                _roll1 = pins;
+            else if(CurrentRole == 2)
+                _roll2 = pins;
+
+            CurrentRole++;
+            return pins;
+        }
+
+        public int GetFrameScore()
+        {
+           return _roll1 + _roll2;
+        }
+
+        public bool IsFrameDone() => CurrentRole > 2;
     }
 }
--- BowlingTests/GameTests.cs
+++ BowlingTests/GameTests.cs
@@ -5,13 +5,34 @@ namespace BowlingTests
     [TestClass]
     public class GameTests
     {
+        private Game _game;
+
+        [TestInitialize]
+        public void SetupGame()
+        {
+            _game = new Game();
+
+        }
+
         [TestMethod]
-        public void RollBall()
+        public void RollingBallResultsIntoScore()
         {
             var game = new Game();
             game.Roll(1);
+            game.Roll(3);
 
-            Assert.AreEqual(1, game.Score);
+            Assert.AreEqual(4, game.Score);
+        }
+
+        [TestMethod]
+        public void TwoRollsPerOpenFrame()
+        {
+            var game = new Game();
+            game.Roll(1);
+            game.Roll(3);
+            game.Roll(4);
+
+            Assert.AreEqual(2, game.CurrentFrame);
         }
     }
 }
